4b9763a17b7005664efa66da6fe1ef53ccb21ce2
Solon <amaral.solon@hotmail.com>
2022-06-21 22:24:43 -0300

    Requisito 4 :dizzy:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+// import Title from './components/Title';
 
 class App extends React.Component {
   render() {
